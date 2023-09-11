6fd4bdd4edc940dfa5847c51b39216b39116cd41
kuldeepsaini23 <kuldeepsaini02311@gmail.com>
2023-09-11 08:58:05 +0530

    Login and Signup

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,24 +1,53 @@
 "use client";
 import Link from "next/link";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
-import {axios} from "axios";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 export default function LoginPage(){
-
+  const router = useRouter();
   const [user, setUser] = useState({
     email:"",
     password:"",
     // username:"",
   })
 
+  const [buttonDisabled, setButtonDisabled] = useState(false);
+
+  const [loading, setLoading] = useState(false);
+
   const onLogin = async ()=>{
-    console.log("Hello World");
+    try{
+      setLoading(true);
+      const response = await axios.post("/api/users/login",user);
+      console.log("Login Success", response.data);
+      toast.success("Login Success");
+      router.push("/profile");
+
+    }catch(error:any){
+      console.group("Login failed", error);
+      toast.error("Login failed");
+    }finally{
+      setLoading(false);
+    }
   }
+
+  useEffect(() => {
+    if (
+      user.email.length > 0 &&
+      user.password.length > 0 
+    ) {
+      setButtonDisabled(false);
+    } else {
+      setButtonDisabled(true);
+    }
+  }, [user]);
+
 
   return(
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1>Login</h1>
+      <h1>{loading ? "Processing" : "Login"}</h1>
       <hr/>
 
       {/* username */}
@@ -40,7 +69,7 @@ export default function LoginPage(){
         value={user.email}
         onChange={(e)=> setUser({...user, email:e.target.value})}
         placeholder="email"
-        className="p-2 border border-gray-200 rounded-lg mb-4 focus:outline-none focus:border-gray-300"
+        className="p-2 border border-gray-200 rounded-lg mb-4 focus:outline-none focus:border-gray-300 text-black"
       />
 
       {/* Password */}
@@ -51,14 +80,14 @@ export default function LoginPage(){
         value={user.password}
         onChange={(e)=> setUser({...user, password:e.target.value})}
         placeholder="password"
-        className="p-2 border border-gray-200 rounded-lg mb-4 focus:outline-none focus:border-gray-300"
+        className="p-2 border border-gray-200 rounded-lg mb-4 focus:outline-none focus:border-gray-300 text-black"
       />
 
 
       <button className="p-2 border border-gray-200 rounded-lg mb-4 focus:outline-none focus:border-gray-300"
           onClick={onLogin}
         >
-        Login here
+        {buttonDisabled ? "No Signup" : "Login here"}
       </button>
 
       <Link href={"/signup"}>Vist Signup Page</Link>
